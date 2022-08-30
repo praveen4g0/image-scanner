8ff145e0cac67428c4266c8b6089df79bdf2d079
praveen4g0 <praveen.thangadancha@gmail.com>
2022-08-30 13:17:57 +0530

    Feat: validation is done now, only if it has required annotation

--- app.py
+++ app.py
@@ -8,23 +8,29 @@ def deployment_webhook():
     request_info = request.get_json()
     uid = request_info["request"]["uid"]
     is_secure = True
-    for each_image in request_info["request"]["object"]["spec"]["template"]["spec"]["containers"]:
-        command = [
-            "/opt/app-root/src/trivy",
-            "image",
-            "-f",
-            "json",
-            "-s",
-            "CRITICAL",
-            "--exit-code",
-            "1",
-            each_image["image"],
-        ]
-        print("Running command: %s" % " ".join(command))
-        r = Popen(command)
-        r.communicate()
-        if r.returncode == 1:
-            is_secure = False
+    if 'scanner.io/owner' in request_info["request"]["object"]["metadata"]["annotations"]:
+        if request_info["request"]["object"]["metadata"]["annotations"]["scanner.io/owner"]=="image-scanner":
+           for each_image in request_info["request"]["object"]["spec"]["template"]["spec"]["containers"]:
+                command = [
+                    "/opt/app-root/src/trivy",
+                    "image",
+                    "-f",
+                    "json",
+                    "-s",
+                    "CRITICAL",
+                    "--exit-code",
+                    "1",
+                    each_image["image"],
+                ]
+                print("Running command: %s" % " ".join(command))
+                r = Popen(command)
+                r.communicate()
+                if r.returncode == 1:
+                    is_secure = False
+        else:
+         return admission_response(True, "skipping validation annotation value didn't match expected: scanner.io/owner: image-scanner", uid)            
+    else:
+     return admission_response(True, "skipping validation as resource doesn't have required annotation", uid)
 
     if is_secure:
         return admission_response(True, "All containers are secure", uid)
@@ -35,23 +41,29 @@ def pod_webhook():
     request_info = request.get_json()
     uid = request_info["request"]["uid"]
     is_secure = True
-    for each_image in request_info["request"]["object"]["spec"]["containers"]:
-        command = [
-            "/opt/app-root/src/trivy",
-            "image",
-            "-f",
-            "json",
-            "-s",
-            "CRITICAL",
-            "--exit-code",
-            "1",
-            each_image["image"],
-        ]
-        print("Running command: %s" % " ".join(command))
-        r = Popen(command)
-        r.communicate()
-        if r.returncode == 1:
-            is_secure = False
+    if 'scanner.io/owner' in request_info["request"]["object"]["metadata"]["annotations"]:
+        if request_info["request"]["object"]["metadata"]["annotations"]["scanner.io/owner"]=="image-scanner":
+           for each_image in request_info["request"]["object"]["spec"]["containers"]:
+                command = [
+                    "/opt/app-root/src/trivy",
+                    "image",
+                    "-f",
+                    "json",
+                    "-s",
+                    "CRITICAL",
+                    "--exit-code",
+                    "1",
+                    each_image["image"],
+                ]
+                print("Running command: %s" % " ".join(command))
+                r = Popen(command)
+                r.communicate()
+                if r.returncode == 1:
+                    is_secure = False
+        else:
+         return admission_response(True, "skipping validation annotation value didn't match expected: scanner.io/owner: image-scanner", uid)            
+    else:
+     return admission_response(True, "skipping validation as resource doesn't have required annotation", uid)
 
     if is_secure:
         return admission_response(True, "All containers are secure", uid)
